bd262801456ed975b613439cfb0747c16d1a7a62
Kazuya Hatano <k28@me.com>
2020-11-13 06:06:12 +0900

    Echoサーバとしての機能を追加
    - /hello で"Hello!"を返す

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,9 @@
 module github.com/k28/ImageUploaderSample/backend
 
 go 1.12
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201112155050-0c6587e931a9 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
-	fmt.Println("Hello")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
+	})
+	e.GET("/hello", hello)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	return c.HTML(http.StatusOK, fmt.Sprintf("Hello!"))
 }
